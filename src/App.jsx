2b9aee7cc095dd3b4eced55f18f685afd4b584ed
Tochukwu Amaechina H <tochukwuamaechina2703@gmail.com>
2023-08-29 16:51:34 +0100

    made few adjustments to the stylings

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
       <main>
         <Header />
         <Aboutme />
-        {/* <Experience /> */}
         <Projects />
+        {/* <Experience /> */}
       </main>
 
       <Footer />
