31455ca0898580f74e6283c71c44e0af4154f9e0
tochi27 <tochukwuamaechina2703@gmail.com>
2024-02-12 15:44:28 +0100

    Added my resume

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./header.module.css";
+import tochiPurple2 from "../assests/tochiPurple2.pdf";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.headerText}>Tochukwu Henry Amaechina (Software Developer)</h1>
+      <h1 className={styles.headerText}>
+        Tochukwu Henry Amaechina (Software Developer)
+      </h1>
       <div className={styles.navItems}>
         <a href="#about-me" className={styles.anchor}>
           <i className={`fa-solid fa-user ${styles.icons}`}></i>
@@ -23,6 +26,19 @@ export default function Header() {
         >
           <i className={`fa-brands fa-github ${styles.icons}`}></i>
         </a>
+      </div>
+      <div className={styles.navItems}>
+        <button>
+          <a
+            className={styles.resume}
+            href={tochiPurple2}
+            target="_blank"
+            rel="noreferrer"
+            download={tochiPurple2.pdf}
+          >
+            My Resume
+          </a>
+        </button>
       </div>
     </header>
   );
